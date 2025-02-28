7de41857ff6efa1128d91261ad6a999eb646a4fd
Imran Chamieh <chamieh.imran@gmail.com>
2025-02-28 10:50:35 +0100

    update

--- mistral.py
+++ mistral.py
@@ -11,7 +11,6 @@ import pandas as pd
 
 api_key = os.getenv("api_key_deepSeek")
 client = OpenAI(api_key, base_url="https://api.deepseek.com")
-##
 
 model = "mistral-large-latest"
 matched_ids = []
